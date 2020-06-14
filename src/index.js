86ca8dc1c6c41bed2c8e9b1a324cc0f23e4a59a6
Kendra Neves <kendraneves@gmail.com>
2020-06-13 21:42:42 -0400

    Sticky footer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import Arrow from './components/arrow';
 import SideBar from './components/sidebar';
-import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/navbar';
+import Footer from './components/footer';
 
 
 
@@ -24,6 +26,7 @@ ReactDOM.render(
       <Arrow arrowColor="#f3f3f3" zIndex="-2" />
       <Arrow arrowColor="#dea6af" zIndex="-3" />
       <App />
+      <Footer />
     </div>
   </React.StrictMode >,
   document.getElementById('root')
