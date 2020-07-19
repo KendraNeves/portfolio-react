f61ae68df5047b7da7120512cd1ed6a7475dc627
Kendra Neves <kendraneves@gmail.com>
2020-07-18 22:25:05 -0400

    Work on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,9 +23,39 @@ const Footer = () => {
         <Row>
           <Col md={3}></Col>
           <Col md={6} className="text-center">
-            <ContactMeIcon />
+            <Icon
+              href="https://github.com/KendraNeves"
+              alt="GitHub icon"
+              iconPic="fab fa-github"
+            />
+            <Icon
+              href="https://www.linkedin.com/in/kendraneves/"
+              alt="Linked In icon"
+              iconPic="fab fa-linkedin"
+            />
+            <Icon
+              href="https://twitter.com/kendraneves"
+              alt="Twitter icon"
+              iconPic="fab fa-twitter"
+            />
           </Col>
           <Col md={3}></Col>
+        </Row>
+        <Row className="mt-5">
+          <Col md={1}></Col>
+          <Col className="text-center"><Icon href="#" alt="Email icon" iconPic="fas fa-envelope" /><p></p></Col>
+          <Col className="text-center" style={{ borderLeft: "1px solid #e6dbc9", borderRight: "1px solid #e6dbc9" }}><Icon href="#" alt="resume icon" iconPic="far fa-file" /></Col>
+          <Col className="text-center">
+            <Icon href="tel:2039485436" alt="phone icon" iconPic="fas fa-mobile-alt" />
+          </Col>
+          <Col md={1}></Col>
+        </Row>
+        <Row className="mb-5">
+          <Col md={1}></Col>
+          <Col className="text-center col-sm-12">{"kendraneves@gmail.com"}</Col>
+          <Col className="text-center col-sm-12">{"Resume"}</Col>
+          <Col className="text-center col-sm-12">{"(203)948-5436"}</Col>
+          <Col md={1}></Col>
         </Row>
       </Container>
       <div className="footer-copyright py-3">
