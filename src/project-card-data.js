e5bdb547998572862356dc3c6f71d1b6530b34a8
Kendra Neves <kendraneves@gmail.com>
2020-07-19 22:21:12 -0400

    Updated card descriptions and messed with layout

--- src/project-card-data.js
+++ src/project-card-data.js
@@ -12,26 +12,82 @@
 
 export const cardList = [
   {
-    "cardTitle": "Nutmeg Concrete",
-    "projectPic": "/images/projects/nutmeg-concrete.PNG",
-    "alt": "nutmeg concrete website",
-    "deployedUrl": "https://nutmegconcrete.com/",
-    "description": "This card has supporting text below as a natural lead-in to additional content",
-    "techStack": "HTML, CSS",
-    "githubUrl": "",
-    "datePosted": "Posted December 11, 2019"
+    "cardTitle": "Curbside",
+    "projectPic": "/images/projects/curbside.PNG",
+    "alt": "Curbside",
+    "deployedUrl": "https://curbsidedeployment.herokuapp.com/",
+    "description": "An eco-conscious app that makes giving away unwanted items easy! Users post the items they no longer want, and those interested pick them up at the curbside.",
+    "techStack": "MERN, MongoDB, Express, React.js, Node.js, Google maps Javascript and Geocode APIs, Passport authentication, Bootstrap",//   
+    "githubUrl": "https://github.com/KendraNeves/curbside",
+  },
+  // Curbside allows users to post items they no longer want and the location that they will be left at. Users looking for items (college students, collectors etc.) can monitor the postings and pick up the items they're interested in at the designated curbside.
+  {
+    "cardTitle": "TP Finder",
+    "projectPic": "/images/projects/tp-finder1.PNG",
+    "alt": "toilet paper finding app",
+    "deployedUrl": "https://vast-sierra-58831.herokuapp.com/",
+    "description": "Full-stack toilet paper finding app created to address shortages caused by the Covid-19 pandemic",
+    "techStack": "MySQL, Node.js, Express, JavaScript, Bootstrap, Passport authentication, Heroku",
+    "githubUrl": "https://github.com/kendraneves/TP-Finder",
+  },
+  {
+    "cardTitle": "Burger App",
+    "projectPic": "/images/projects/burger-app.PNG",
+    "alt": "burger app",
+    "deployedUrl": "https://calm-lake-01040.herokuapp.com/",
+    "description": "Full-stack app that allows users to add and store about delicious burgers",
+    "techStack": "Handlebars, JavaScript, mySQL Node.js, Bootstrap, Express, Heroku",
+    "githubUrl": "https://github.com/kendraneves/Burger-App",
+  },
+  {
+    "cardTitle": "Good Morning App",
+    "projectPic": "/images/projects/good-morning-app.PNG",
+    "alt": "good morning app",
+    "deployedUrl": "https://kendraneves.github.io/Good-Morning/",
+    "description": "An uplifting app that greets users with the date, the local weather, a gif about the weather, and an inspirational quote",
+    "techStack": "JavaScript, jQuery, AJAX, Materialize CSS, Open Weather API, Giphy API, Quotes API, HTML, CSS",
+    "githubUrl": "https://github.com/KendraNeves/Good-Morning",
+  },
+  {
+    "cardTitle": "Work Day Scheduler",
+    "projectPic": "/images/projects/work-day-scheduler.PNG",
+    "alt": "work day scheduler app",
+    "deployedUrl": "https://kendraneves.github.io/Work-Day-Scheduler/",
+    "description": "App that adds and stores events in a user's schedule. Features include efficient local storage management and time-tracking functionality.",
+    "techStack": "Javascript, HTML, CSS, Bootstrap, Moment.js, jQuery, local storage",
+    "githubUrl": "https://github.com/KendraNeves/Work-Day-Scheduler",
+  },
+  {
+    "cardTitle": "Weather Dashboard",
+    "projectPic": "/images/projects/weather-dashboard.PNG",
+    "alt": "weather dashboard app",
+    "deployedUrl": "https://kendraneves.github.io/Weather-Dashboard/",
+    "description": "Hello World!",
+    "techStack": "Javascript, AJAX calls, Bootstrap, jQuery, Moment.js, HTML, CSS",
+    "githubUrl": "https://github.com/KendraNeves/Weather-Dashboard",
   },
 
-  {
-    "cardTitle": "Password Generator",
-    "projectPic": "/images/projects/password-generator.PNG",
-    "alt": "password generator website",
-    "deployedUrl": "https://kendraneves.github.io/Password-Generator/",
-    "description": "Hello World!",
-    "techStack": "JavaScript, HTML, CSS, DOM traversal",
-    "githubUrl": "https://github.com/KendraNeves/Password-Generator",
-    "datePosted": "Posted February 14, 2020"
-  },
+  // {
+  //   "cardTitle": "Nutmeg Concrete",
+  //   "projectPic": "/images/projects/nutmeg-concrete.PNG",
+  //   "alt": "nutmeg concrete website",
+  //   "deployedUrl": "https://nutmegconcrete.com/",
+  //   "description": "This card has supporting text below as a natural lead-in to additional content",
+  //   "techStack": "HTML, CSS",
+  //   "githubUrl": "",
+  //   "datePosted": "Posted December 11, 2019"
+  // },
+
+  // {
+  //   "cardTitle": "Password Generator",
+  //   "projectPic": "/images/projects/password-generator.PNG",
+  //   "alt": "password generator website",
+  //   "deployedUrl": "https://kendraneves.github.io/Password-Generator/",
+  //   "description": "Hello World!",
+  //   "techStack": "JavaScript, HTML, CSS, DOM traversal",
+  //   "githubUrl": "https://github.com/KendraNeves/Password-Generator",
+  //   "datePosted": "Posted February 14, 2020"
+  // },
 
   // {
   //   "cardTitle": "Code Quiz",
@@ -44,38 +100,8 @@ export const cardList = [
   //   "datePosted": "Posted February 21, 2020"
   // },
 
-  {
-    "cardTitle": "Work Day Scheduler",
-    "projectPic": "/images/projects/work-day-scheduler.PNG",
-    "alt": "work day scheduler app",
-    "deployedUrl": "https://kendraneves.github.io/Work-Day-Scheduler/",
-    "description": "Hello World!",
-    "techStack": "Javascript, HTML, CSS, Bootstrap, Moment.js, jQuery, local storage",
-    "githubUrl": "https://github.com/KendraNeves/Work-Day-Scheduler",
-    "datePosted": "Posted February 29, 2020"
-  },
 
-  {
-    "cardTitle": "Weather Dashboard",
-    "projectPic": "/images/projects/weather-dashboard.PNG",
-    "alt": "weather dashboard app",
-    "deployedUrl": "https://kendraneves.github.io/Weather-Dashboard/",
-    "description": "Hello World!",
-    "techStack": "Javascript, AJAX calls, Bootstrap, jQuery, Moment.js, HTML, CSS",
-    "githubUrl": "https://github.com/KendraNeves/Weather-Dashboard",
-    "datePosted": "Posted March 9, 2020"
-  },
 
-  {
-    "cardTitle": "Good Morning App",
-    "projectPic": "/images/projects/good-morning-app.PNG",
-    "alt": "good morning app",
-    "deployedUrl": "https://kendraneves.github.io/Good-Morning/",
-    "description": "Hello World!",
-    "techStack": "JavaScript, jQuery, AJAX, Materialize CSS, Open Weather API, Giphy API, Quotes API, HTML, CSS",
-    "githubUrl": "https://github.com/KendraNeves/Good-Morning",
-    "datePosted": "Posted March 11, 2020"
-  },
 
   // {
   //   "cardTitle": "README Generator",
@@ -99,60 +125,32 @@ export const cardList = [
   //   "datePosted": "Posted April 5, 2020"
   // },
 
-  {
-    "cardTitle": "Note Taking App",
-    "projectPic": "/images/projects/note-taker2.PNG",
-    "alt": "note taking app",
-    "deployedUrl": "https://secret-springs-62228.herokuapp.com/notes",
-    "description": "Hello World!",
-    "techStack": "",
-    "githubUrl": "https://github.com/kendraneves/Express-Note-Taker",
-    "datePosted": "Posted April 13, 2020"
-  },
-
-  {
-    "cardTitle": "Employee Tracker",
-    "projectPic": "",
-    "alt": "employee tracking app",
-    "deployedUrl": "https://github.com/kendraneves/Employee-Tracker",
-    "description": "Hello World!",
-    "techStack": "",
-    "githubUrl": "https://github.com/kendraneves/Employee-Tracker",
-    "datePosted": "Posted April 20, 2020"
-  },
-
-  {
-    "cardTitle": "Burger App",
-    "projectPic": "/images/projects/burger-app.PNG",
-    "alt": "burger app",
-    "deployedUrl": "https://calm-lake-01040.herokuapp.com/",
-    "description": "Hello World!",
-    "techStack": "Node.js, Express router, JavaScript, mySQL",
-    "githubUrl": "https://github.com/kendraneves/Node-Express-Handlebars",
-    "datePosted": "Posted April 26, 2020"
-  },
-
-  {
-    "cardTitle": "TP Finder",
-    "projectPic": "/images/projects/tp-finder1.PNG",
-    "alt": "toilet paper finding app",
-    "deployedUrl": "https://vast-sierra-58831.herokuapp.com/",
-    "description": "Hello World!",
-    "techStack": "Node.js, Express router, JavaScript, Passport authentication",
-    "githubUrl": "https://github.com/kendraneves/TP-Finder",
-    "datePosted": "Posted May 8, 2020"
-  },
+  // {
+  //   "cardTitle": "Note Taking App",
+  //   "projectPic": "/images/projects/note-taker2.PNG",
+  //   "alt": "note taking app",
+  //   "deployedUrl": "https://secret-springs-62228.herokuapp.com/notes",
+  //   "description": "Hello World!",
+  //   "techStack": "",
+  //   "githubUrl": "https://github.com/kendraneves/Express-Note-Taker",
+  //   "datePosted": "Posted April 13, 2020"
+  // },
 
   // {
-  //   "cardTitle":"",
+  //   "cardTitle": "Employee Tracker",
   //   "projectPic": "",
-  //   "alt": "",
-  //   "deployedUrl": "",
-  //   "description": "",
-  // "techStack": "",//   
-  // "githubUrl": "",
-  // //   "datePosted": ""
-  // // },
+  //   "alt": "employee tracking app",
+  //   "deployedUrl": "https://github.com/kendraneves/Employee-Tracker",
+  //   "description": "Hello World!",
+  //   "techStack": "",
+  //   "githubUrl": "https://github.com/kendraneves/Employee-Tracker",
+  //   "datePosted": "Posted April 20, 2020"
+  // },
+
+
+
+
+
 
   // // {
   // //   "cardTitle":"",
